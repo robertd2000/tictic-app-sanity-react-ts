7a4bf5b7944d4333747c295036e3a9ac8629a56e
Rob <rob@example.com>
2022-07-20 23:05:23 +0300

    add search and categories features

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,9 @@ const Home = ({ videos }: IProps) => {
   return (
     <div className="flex flex-col gap-10 videos h-full">
       {videos.length ? (
-        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+        videos?.map((video: Video) => (
+          <VideoCard post={video} key={video._id} />
+        ))
       ) : (
         <NoResults text="No videos" />
       )}
@@ -20,14 +22,20 @@ const Home = ({ videos }: IProps) => {
   )
 }
 
-export const getServerSideProps = async () => {
-  const { data } = await axios.get(`${BASE_URL}/api/post`)
+export default Home
+
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string }
+}) => {
+  let response = await axios.get(`${BASE_URL}/api/post`)
+
+  if (topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`)
+  }
 
   return {
-    props: {
-      videos: data,
-    },
+    props: { videos: response.data },
   }
 }
-
-export default Home
